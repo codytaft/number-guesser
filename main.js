17ce94ebb7035658997e13966187cb09a878c151
Cody Taft <cody.taft@gmail.com>
2018-05-09 17:13:53 -0600

    Add min max input functionality. Add submit button in HTML.

--- main.js
+++ main.js
@@ -1,40 +1,74 @@
-var randomNumber = parseInt(Math.floor(Math.random() * 100), 10);
+var randomNumber = Math.floor(Math.random() * 100);
 var theGuess = document.querySelector('#guess');
-var inputForm = document.querySelector('.theForm');
+var theGuessValue = parseInt(theGuess.value);
+var inputForm = document.querySelector('theForm');
 var prevGuess = document.querySelector('#lastGuess');
 var guessBtn = document.querySelector('#guess-btn');
 var clearBtn = document.querySelector('#clear-btn');
 var resetBtn = document.querySelector('#reset');
 var numResult = document.querySelector('#highLow');
+var textChange = document.querySelector('#yourLast');
+var wrongRange = document.querySelector('#out-range');
+var userMin = document.querySelector('#min');
+var userMax = document.querySelector('#max');
+var max = 100;
+var min = 0;
+// var randomNumber = userField (userMin, userMax);
 
+submitRange.addEventListener('click', userField);
 theGuess.addEventListener('keydown', pressingEnter);
 theGuess.addEventListener('keyup', toggleButton);
+theGuess.addEventListener('keypress', onlyNumbers);
+theGuess.addEventListener('keydown', outRange);
 guessBtn.addEventListener('click', displayAnswer);
 guessBtn.addEventListener('click', lastGuess);
 clearBtn.addEventListener('click', clearInput);
-
 resetBtn.addEventListener('click', function() {
   location.reload(true);
 });
 
+
+function outRange () {
+  if (theGuess.value > max || theGuess.value < min) {
+    wrongRange.innerText = 'That is out of the range';
+  }
+}
+
+function userField () {
+  max = parseInt(userMax.value);
+  min = parseInt(userMin.value);
+  randomNumber = parseInt(Math.random() * (max - min) + min);
+}
+
+
+function onlyNumbers (event) {
+  var charCode = (event.which) ? event.which : event.keyCode
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    theGuess.value = ''
+    wrongRange.innerText = 'That is not a number, Hal!';
+  }
+}
+
 function clearInput () {
   theGuess.value='';
+  wrongRange.innerText = '';
 }
 
 /displays last guess/
 function lastGuess () {
-  prevGuess.innerText = theGuess.value;
   event.preventDefault();
-}
+  textChange.innerText = 'Your last guess was';
+  prevGuess.innerText = theGuess.value;
+} 
 
 /displays correct or high or low value/
 function displayAnswer () {
     if (randomNumber < theGuess.value) {
-     numResult.innerHTML = 'That is too high';
+     numResult.innerText = 'That is too high';
   } else if (randomNumber > theGuess.value) {
-     numResult.innerHTML = 'That is too low';
+     numResult.innerText = 'That is too low';
   } else {
-     numResult.innerHTML = 'BOOM!';
+     numResult.innerText = 'BOOM!';
   }
 };
 
@@ -45,7 +79,7 @@ function pressingEnter(event) {
   }
 }
 function toggleButton () {
-  if (theGuess.value === '') {
+  if (theGuess.value === '')  {
     guessBtn.disabled = true;
     clearBtn.disabled = true;
     resetBtn.disabled = true;
@@ -55,8 +89,5 @@ function toggleButton () {
     resetBtn.disabled = false;
   }
 }
-
-
-
 
 
